57ed4dc099816e69070ebf5ebb898d8ae972598e
Stanimir Stoev <stoevs@gmail.com>
2016-06-22 20:43:04 +0300

    added tests and comments

--- HOMEWORK/03.ExtensionsDelegatesLambdaLINQ/Extensions/Startup.cs
+++ HOMEWORK/03.ExtensionsDelegatesLambdaLINQ/Extensions/Startup.cs
@@ -1,12 +1,17 @@
 ﻿namespace Problems1to2
 {
     using System;
+    using System.Collections.Generic;
     using System.Text;
 
     class Startup
     {
         static void Main()
         {
+
+            //Problem 1. Implement an extension method Substring(int index, int length) for the class StringBuilder
+            //that returns new StringBuilder and has the same functionality as Substring in the class String.
+
             var testSBSubstring = new StringBuilder();
 
             testSBSubstring.Append("I want to remove this. Show only this.");
@@ -14,6 +19,24 @@
             Console.WriteLine("----------------------");
             StringBuilder testAfterSubstring = testSBSubstring.Substring(23, 15);
             Console.WriteLine(testAfterSubstring);
+
+            //Problem 2. IEnumerable extensions 
+            //Implement a set of extension methods for IEnumerable<T> that implement
+            //the following group functions: sum, product, min, max, average.
+            //Testing of the various extensions for different types of collections and elements:
+
+            IEnumerable<int> test = new List<int>() { 1, 2, 3, 4, 5 };
+            Console.WriteLine(test.SumExtension());
+            IEnumerable<double> test1 = new[] { 1.5, 2.5, 3.5, 4.5, 5.5 };
+
+            Console.WriteLine(test1.SumExtension());
+            Console.WriteLine(test1.ProductExtension());
+            Console.WriteLine(test1.MinExtension());
+            Console.WriteLine(test1.MaxExtension());
+            Console.WriteLine(test1.AverageExtension());
+
+            Console.WriteLine(MyExtensions.AverageExtension(test));
+            Console.WriteLine(MyExtensions.ProductExtension(test1));
         }
     }
 }
